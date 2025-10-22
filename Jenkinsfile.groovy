b7112c8d08074b14479d06380fe3b2f8a0c4df20
AbdulAhad20e <abdulahadCXL123@gmail.com>
2025-10-22 23:41:33 +0500

    Add files via upload

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,54 @@
+pipeline {
+    agent any
+    
+    tools {
+        maven 'Maven-3.9.11'
+    }
+    
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'master', 
+                url: 'https://github.com/kriru/firstJava.git',
+                credentialsId: ''  // Remove this line if no credentials needed
+            }
+        }
+        
+        stage('Build') {
+            steps {
+                bat 'mvn clean compile'
+            }
+        }
+        
+        stage('Static Analysis') {
+            steps {
+                bat 'mvn findbugs:findbugs pmd:pmd checkstyle:checkstyle'
+            }
+            
+            post {
+                always {
+                    // This will always run, even if static analysis fails
+                    echo 'Static analysis completed'
+                }
+            }
+        }
+        
+        stage('Test') {
+            steps {
+                bat 'mvn test'
+            }
+        }
+    }
+    
+    post {
+        always {
+            echo 'Pipeline completed'
+        }
+        success {
+            echo 'Pipeline succeeded!'
+        }
+        failure {
+            echo 'Pipeline failed!'
+        }
+    }
+}
